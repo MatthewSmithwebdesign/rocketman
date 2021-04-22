2b9c80b705404920e953413314278cddce1d8ec9
Matthew Smith <matthewsmithwebdesign@gmail.com>
2021-04-21 19:20:49 -0700

    added a listblock

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,4 +1,5 @@
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 class TitleBlock(blocks.StructBlock):
     text = blocks.CharBlock(
@@ -11,3 +12,21 @@ class TitleBlock(blocks.StructBlock):
         icon = " edit"
         label = "Title"
         help_text = " Center text to display"
+
+class CardsBlock (blocks.StructBlock):
+
+    cards= blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("title", blocks.CharBlock(max_length=100, help_text="Bold text for this card max length of 100 characters.")),
+                ("text", blocks.TextBlock(max_length=355, help_text="Optional text for the card. Max length is 355 characters.", required=False)),
+                ("image", ImageChooserBlock(help_text="Image will me auto cropped to 570px by 370px")),
+            ]
+        )
+    )
+    class Meta:
+        template = "streams/cards_block.html"
+        icon = "image"
+        label = "Standard Cards"
+
+
